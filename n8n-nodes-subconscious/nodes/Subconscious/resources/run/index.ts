e091167efefe053d3a184c54c63d828f0ac5a343
Owen Stepan <106773727+ostepan8@users.noreply.github.com>
2026-02-03 15:25:14 -0500

    remove

--- n8n-nodes-subconscious/nodes/Subconscious/resources/run/index.ts
+++
@@ -1,6 +0,0 @@
-import type { INodeProperties } from 'n8n-workflow';
-import { runCreateDescription } from './create';
-
-export const runDescription: INodeProperties[] = [
-	...runCreateDescription,
-];
